58ece5751f4b0f4a332d3f7c017e60d212189937
yama-sitter <dadayamism@gmail.com>
2023-07-17 22:51:08 +0900

    install neovim and the plugin manager lazy.nvim and make the help Japanese

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+vim.opt.number = true
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.opt.ambiwidth = 'double'
+vim.opt.clipboard:append({unnamedplus = true})
+vim.opt.helplang = 'ja,en'
+
