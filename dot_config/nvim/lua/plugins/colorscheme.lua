e19a3e6f84717936bf1a74a0bcdce575d4c4e3eb
yama-sitter <dadayamism@gmail.com>
2023-07-18 18:42:04 +0900

    add colorscheme called "nord"

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'shaunsingh/nord.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd[[colorscheme nord]]
+
+      vim.g.nord_disable_background = true
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_italic = false
+      vim.g.nord_bold = false
+
+      require('nord').set()
+    end,
+  }
+}
