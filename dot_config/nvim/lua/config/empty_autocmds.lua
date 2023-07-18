5aa491d7e5c08b7b493fdd09ceef8aa8695669a8
yama-sitter <dadayamism@gmail.com>
2023-07-18 21:35:47 +0900

    transfer monitoring of files under chezmoi management to chezmoi instead of vim

--- dot_config/nvim/lua/config/empty_autocmds.lua
+++
@@ -1,6 +0,0 @@
--- Editing a file managed by chezmoi will execute "apply" command
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-  pattern = '~/.local/share/chezmoi/*',
-  command = 'chezmoi apply --source-path %',
-})
-
