e827b4d573a1f5431e586d17a153cc3869916374
yama-sitter <dadayamism@gmail.com>
2023-07-19 23:05:44 +0900

    added key mapping for gitsigns

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -12,10 +12,36 @@ return {
         },
         signcolumn = false,
         numhl = true,
+
         current_line_blame = true,
         current_line_blame_opts = {
           delay = 300,
         },
+
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+        end,
       }
     end,
     event = { 'VeryLazy' },
