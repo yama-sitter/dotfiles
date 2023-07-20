149b2254139b1dfe3c01a3516b3847a136341286
yama-sitter <dadayamism@gmail.com>
2023-07-20 14:06:38 +0900

    added 'apply' command of chezmoi to the group

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,7 @@
 -- Execute 'apply' when saving files under the chezmoi directory
+vim.api.nvim_create_augroup('ChezmoiApply', {})
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = 'ChezmoiApply',
   pattern = { '*.local/share/chezmoi/*' },
   command = [[ !chezmoi apply --source-path "%" ]],
 })
