fb37bc939db1acb65a31d9480ef3fb2392dcb481
yama-sitter <dadayamism@gmail.com>
2023-07-20 21:47:59 +0900

    move gitsigns options to opts

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -2,50 +2,48 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     event = { 'VeryLazy' },
-    config = function()
-      require('gitsigns').setup {
-        signs = {
-          change       = { numhl = 'GitSignsChangeNr' },
-          delete       = { numhl = 'GitSignsDeleteNr' },
-          topdelete    = { numhl = 'GitSignsDeleteNr' },
-          changedelete = { numhl = 'GitSignsChangeNr' },
-          untracked    = { numhl = 'GitSignsAddNr'    },
-        },
-        signcolumn = false,
-        numhl = true,
+    opts = {
+      signs = {
+        change       = { numhl = 'GitSignsChangeNr' },
+        delete       = { numhl = 'GitSignsDeleteNr' },
+        topdelete    = { numhl = 'GitSignsDeleteNr' },
+        changedelete = { numhl = 'GitSignsChangeNr' },
+        untracked    = { numhl = 'GitSignsAddNr'    },
+      },
+      signcolumn = false,
+      numhl = true,
 
-        current_line_blame = true,
-        current_line_blame_opts = {
-          delay = 300,
-        },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 300,
+      },
 
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
 
-          map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
-            return '<Ignore>'
-          end, {expr=true})
-          map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
-            return '<Ignore>'
-          end, {expr=true})
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
 
-          map('n', '<leader>hd', gs.diffthis)
-          map('n', '<leader>hl', gs.setqflist)
-          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-        end,
-      }
-    end,
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hl', gs.setqflist)
+        map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+        map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+      end,
+    },
   },
 }
 
