201bc0b07760ceeabf81ce07f9907aa57c3a1eb2
yama-sitter <dadayamism@gmail.com>
2023-07-23 09:26:16 +0900

    install tag and comment setting

---
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,12 @@
+return {
+  'numToStr/Comment.nvim',
+  event = 'VeryLazy',
+  opts = {
+    toggler = {
+      line = '<leader>\\\\',
+    },
+    opleader = {
+      block = '<leader>\\\\',
+    },
+  },
+}
