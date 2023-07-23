d6ddab911de499bc02d41984293699ca32b5e7b2
yama-sitter <dadayamism@gmail.com>
2023-07-24 00:47:06 +0900

    install inc-rename

---
+++ dot_config/nvim/lua/plugins/inc-rename.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'smjonas/inc-rename.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('inc_rename').setup()
+
+      vim.keymap.set('n', '<leader>rn', function()
+        return ':IncRename ' .. vim.fn.expand('<cword>')
+      end, { expr = true })
+    end,
+  },
+}
+
