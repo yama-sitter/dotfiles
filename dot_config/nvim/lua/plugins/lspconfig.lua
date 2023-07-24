d96f857e5b6086b101d0b594d3f9b8ca5ce3dae7
yama-sitter <dadayamism@gmail.com>
2023-07-24 18:43:33 +0900

    install lint and format environment to javascript

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,20 @@ local settings = {
   },
 }
 
+local eslint = {
+  lintCommand = './node_modules/.bin/eslint --stdin --stdin-filename ${INPUT}',
+  lintIgnoreExitCode = true,
+  lintStdin = true,
+  lintFormats = {
+    '%f(%l,%c): %tarning %m',
+    '%f(%l,%c): %rror %m',
+  },
+}
+local prettier = {
+  formatCommand = './node_modules/.bin/prettier --stdin-filepath ${INPUT}',
+  formatStdin = true,
+}
+
 return {
   {
     'williamboman/mason.nvim',
@@ -16,7 +30,7 @@ return {
       'neovim/nvim-lspconfig',
       'williamboman/mason-lspconfig',
       'hrsh7th/cmp-nvim-lsp',
-      'kkharji/lspsaga.nvim',
+      'mattn/efm-langserver',
     },
     config = function()
       require('mason').setup {}
@@ -29,15 +43,56 @@ return {
         },
         automatic_installation = true,
       }
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       mason_lspconfig.setup_handlers {
         function(server_name)
           require('lspconfig')[server_name].setup {
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
+            capabilities = capabilities,
             settings = settings[server_name],
           }
+        end
+      }
+
+      require('lspconfig').efm.setup {
+        init_options = { documentFormatting = true },
+        filetypes = {
+          'typescript',
+          'typescriptreact',
+          'javascript',
+          'javacriptreact',
+        },
+        settings = {
+          rootMarkers = { '.git/' },
+          languages = {
+            javascript = { eslint, prettier },
+            javascriptreact = { eslint, prettier },
+            typescript = { eslint, prettier },
+            typescriptreact = { eslint, prettier },
+          },
+        },
+        on_attach = function(client, bufnr)
+          vim.keymap.set('n', '<leader>m', '<cmd>lua vim.lsp.buf.format()<CR>')
+
+          -- Format on save
+          if (client.server_capabilities.documentFormattingProvider) then
+            vim.api.nvim_create_augroup('AutoFormat', {})
+            vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+              group = 'AutoFormat',
+              pattern = {
+                '*.ts',
+                '*.tsx',
+                '*.js',
+                '*.jsx',
+              },
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = true })
+              end,
+            })
+          end
         end,
       }
     end,
   },
 }
-
