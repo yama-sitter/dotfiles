19a6396e428b0af6d8c8d1314efa51c62195f461
yama-sitter <dadayamism@gmail.com>
2023-08-30 20:50:53 +0900

    refactor: Update key mappings for camelcase-motion and smartword

--- dot_config/nvim/lua/plugins/camelcase-motion.lua
+++ dot_config/nvim/lua/plugins/camelcase-motion.lua
@@ -9,14 +9,10 @@ return {
       vim.g.camelcasemotion_key = '<leader>'
 
       local options = { silent = true }
-      vim.keymap.set('n', 'w', '<Plug>CamelCaseMotion_w', options)
-      vim.keymap.set('n', 'b', '<Plug>CamelCaseMotion_b', options)
-      vim.keymap.set('n', 'e', '<Plug>CamelCaseMotion_e', options)
-      vim.keymap.set('n', 'ge', '<Plug>CamelCaseMotion_ge', options)
-      vim.keymap.set('v', 'w', '<Plug>CamelCaseMotion_w', options)
-      vim.keymap.set('v', 'b', '<Plug>CamelCaseMotion_b', options)
-      vim.keymap.set('v', 'e', '<Plug>CamelCaseMotion_e', options)
-      vim.keymap.set('v', 'ge', '<Plug>CamelCaseMotion_ge', options)
+      vim.keymap.set('', 'w', '<Plug>CamelCaseMotion_w', options)
+      vim.keymap.set('', 'b', '<Plug>CamelCaseMotion_b', options)
+      vim.keymap.set('', 'e', '<Plug>CamelCaseMotion_e', options)
+      vim.keymap.set('', 'ge', '<Plug>CamelCaseMotion_ge', options)
     end,
   },
 }
