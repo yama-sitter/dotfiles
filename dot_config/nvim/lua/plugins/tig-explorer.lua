053e39c6c431b1e37a1c5eb5b075cdfa7dce8bab
yama-sitter <dadayamism@gmail.com>
2023-09-07 19:57:08 +0900

    TigExplorerにsilentオプションを追加

--- dot_config/nvim/lua/plugins/tig-explorer.lua
+++ dot_config/nvim/lua/plugins/tig-explorer.lua
@@ -6,7 +6,7 @@ return {
       'rbgrouleff/bclose.vim',
     },
     config = function ()
-      local options = { noremap = true }
+      local options = { noremap = true, silent = true }
       vim.keymap.set('n', '<leader>ts', ':TigStatus<CR>', options)
       vim.keymap.set('n', '<leader>tb', ':TigBlame<CR>', options)
       vim.keymap.set('n', '<leader>to', ':TigOpenCurrentFile<CR>', options)
