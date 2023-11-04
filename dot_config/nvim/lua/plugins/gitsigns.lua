6aab9b01efd6fe46a32203f08a62c4b08210626b
yama-sitter <dadayamism@gmail.com>
2023-11-04 16:59:03 +0900

    gitsigns.luaのキーマップを修正

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -27,12 +27,12 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
-        map('n', ']c', function()
+        map('n', '<C-]>', function()
           if vim.wo.diff then return ']c' end
           vim.schedule(function() gs.next_hunk() end)
           return '<Ignore>'
         end, { expr = true })
-        map('n', '[c', function()
+        map('n', '<C-[>', function()
           if vim.wo.diff then return '[c' end
           vim.schedule(function() gs.prev_hunk() end)
           return '<Ignore>'
