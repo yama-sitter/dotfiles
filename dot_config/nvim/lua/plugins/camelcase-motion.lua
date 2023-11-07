347cc52ad0f613c514775dc028f746cd3248b2c1
yama-sitter <dadayamism@gmail.com>
2023-11-07 18:24:55 +0900

    体験が悪いのでcamelcase-motionをdisabledに設定

--- dot_config/nvim/lua/plugins/camelcase-motion.lua
+++ dot_config/nvim/lua/plugins/camelcase-motion.lua
@@ -1,18 +1,17 @@
 return {
-  {
-    'bkad/CamelCaseMotion',
-    event = 'VeryLazy',
-    dependencies = {
-      'kana/vim-smartword',
-    },
-    config = function()
-      vim.g.camelcasemotion_key = '<leader>'
-
-      local options = { silent = true }
-      vim.keymap.set('', 'w', '<Plug>CamelCaseMotion_w', options)
-      vim.keymap.set('', 'b', '<Plug>CamelCaseMotion_b', options)
-      vim.keymap.set('', 'e', '<Plug>CamelCaseMotion_e', options)
-      vim.keymap.set('', 'ge', '<Plug>CamelCaseMotion_ge', options)
-    end,
+  'bkad/CamelCaseMotion',
+  enabled = false,
+  event = 'VeryLazy',
+  dependencies = {
+    'kana/vim-smartword',
   },
+  config = function()
+    vim.g.camelcasemotion_key = '<leader>'
+
+    local options = { silent = true }
+    vim.keymap.set('', 'w', '<Plug>CamelCaseMotion_w', options)
+    vim.keymap.set('', 'b', '<Plug>CamelCaseMotion_b', options)
+    vim.keymap.set('', 'e', '<Plug>CamelCaseMotion_e', options)
+    vim.keymap.set('', 'ge', '<Plug>CamelCaseMotion_ge', options)
+  end,
 }
--- dot_config/nvim/lua/plugins/smartword.lua
+++ dot_config/nvim/lua/plugins/smartword.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    'kana/vim-smartword',
-    event = 'VeryLazy',
-    config = function()
-      local options = { noremap = true }
-      vim.keymap.set('', 'w', '<Plug>(smartword-w)', options)
-      vim.keymap.set('', 'b', '<Plug>(smartword-b)', options)
-      vim.keymap.set('', 'e', '<Plug>(smartword-e)', options)
-      vim.keymap.set('', 'ge', '<Plug>(smartword-ge)', options)
-    end,
-  },
+  'kana/vim-smartword',
+  event = 'VeryLazy',
+  config = function()
+    local options = { noremap = true }
+    vim.keymap.set('', 'w', '<Plug>(smartword-w)', options)
+    vim.keymap.set('', 'b', '<Plug>(smartword-b)', options)
+    vim.keymap.set('', 'e', '<Plug>(smartword-e)', options)
+    vim.keymap.set('', 'ge', '<Plug>(smartword-ge)', options)
+  end,
 }
