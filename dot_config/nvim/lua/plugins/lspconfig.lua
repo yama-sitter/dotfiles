1da38ee61e0b086d5ebe9b687b28acfbb3948992
yama-sitter <dadayamism@gmail.com>
2023-11-07 18:18:53 +0900

    vimのオートフォーマットが効かなかったので修正

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -97,12 +97,11 @@ return {
 
           -- Format on save
           if (client.server_capabilities.documentFormattingProvider) then
-            vim.api.nvim_create_augroup('AutoFormat', {})
-            vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-              group = 'AutoFormat',
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = vim.api.nvim_create_augroup('AutoFormat', { clear = true }),
               buffer = bufnr,
               callback = function()
-                vim.lsp.buf.format()
+                vim.lsp.buf.format({ bufnr = bufnr })
               end,
             })
           end
