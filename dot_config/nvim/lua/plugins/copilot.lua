f9672ac8b25e11a1d1f70aa17d824ae496509a0c
yama-sitter <dadayamism@gmail.com>
2023-11-13 19:14:41 +0900

    copilotの導入

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  'github/copilot.vim',
+  lazy = false,
+  config = function()
+    -- Disable tab mapping
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+
+    vim.keymap.set('i', '<C-g>', 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true, replace_keycodes = false })
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', opts)
+    vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', opts)
+    vim.keymap.set('i', '<C-o>', '<Plug>(copilot-dismiss)', opts)
+    vim.keymap.set('i', '<C-s>', '<Plug>(copilot-suggest)', opts)
+  end
+}
