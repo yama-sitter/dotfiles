f3c2b549778c5275fb147365e3950aede239f235
yama-sitter <dadayamism@gmail.com>
2023-11-25 17:01:35 +0900

    vimのバッファでターミナルを開いた時に、<C-q＞で消せるようにキーマッピングを修正

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -10,7 +10,15 @@ map('n', '<leader><esc>', ':nohlsearch<cr>', options)
 -- Buffers
 map('n', '<C-l>', ':bn<CR>', options)
 map('n', '<C-h>', ':bp<CR>', options)
-map('n', '<C-q>', ':bw<CR>', options)
+
+function vim.CloseBuffer()
+  if vim.bo.buftype == 'terminal' then
+    vim.cmd('bw!')
+  else
+    vim.cmd('bw')
+  end
+end
+map('n', '<C-q>', vim.CloseBuffer, options)
 
 -- Copy name of current file to clipboard
 map('n', '<leader>cp', ':let @* = expand("%:p")<CR>', options)
