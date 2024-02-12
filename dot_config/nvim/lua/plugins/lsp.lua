a90a971b2e34b112482a3f74c9a9ee7d79c55a0a
yama-sitter <dadayamism@gmail.com>
2024-02-12 18:37:35 +0900

    LSPの設定をefm-langserverからnone-lsに変更

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,117 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			require("mason").setup()
+
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"ruby_ls",
+				},
+				automatic_installation = true,
+			})
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local settings = {
+				lua_ls = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			}
+
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						settings = settings[server_name],
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"jay-babu/mason-null-ls.nvim",
+		},
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("mason").setup()
+
+			require("mason-null-ls").setup({
+				ensure_installed = { "cspell", "stylua", "markuplint", "eslint-lsp", "prettier" },
+				automatic_installation = true,
+			})
+
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.diagnostics.cspell.with({
+						config = {
+							find_json = function(cwd)
+								return vim.fn.expand(cwd .. "/.vscode/cspell.json")
+							end,
+						},
+						diagnostics_postprocess = function(diagnostic)
+							diagnostic.severity = vim.diagnostic.severity["WARN"]
+						end,
+						condition = function()
+							return vim.fn.executable("cspell") > 0
+						end,
+					}),
+					null_ls.builtins.formatting.stylua.with({
+						condition = function()
+							return vim.fn.executable("stylua") > 0
+						end,
+					}),
+					null_ls.builtins.diagnostics.markuplint.with({
+						filetypes = { "html", "javascriptreact", "typescriptreact" },
+						prefer_local = "node_modules/.bin",
+						condition = function()
+							return vim.fn.executable("markuplint") > 0
+						end,
+					}),
+					null_ls.builtins.diagnostics.eslint.with({
+						prefer_local = "node_modules/.bin",
+					}),
+					null_ls.builtins.formatting.prettier.with({
+						prefer_local = "node_modules/.bin",
+					}),
+				},
+				on_attach = function(client, bufnr)
+					-- Format on save
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+
+					-- Disable inline diagnostics
+					vim.diagnostic.config({
+						virtual_text = false,
+					})
+
+					vim.keymap.set("n", "<leader>fm", "<cmd>lua vim.lsp.buf.format()<CR>", { buffer = bufnr })
+				end,
+			})
+		end,
+	},
+}
