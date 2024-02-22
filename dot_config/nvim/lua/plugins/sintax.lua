612a2b630bbfdcc192889a4ff263e9e7e5246e58
yama-sitter <dadayamism@gmail.com>
2024-02-22 17:47:27 +0900

    space -> tab

--- dot_config/nvim/lua/plugins/sintax.lua
+++ dot_config/nvim/lua/plugins/sintax.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    -- Highlight the current word under the cursor
-    "RRethy/vim-illuminate",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("illuminate").configure()
-    end,
-  },
+	{
+		-- Highlight the current word under the cursor
+		"RRethy/vim-illuminate",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("illuminate").configure()
+		end,
+	},
 }
