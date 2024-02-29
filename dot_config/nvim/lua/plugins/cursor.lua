ccff4fb90e21f272b7024be64801c473e37d4d5f
yama-sitter <dadayamism@gmail.com>
2024-03-01 00:43:19 +0900

    カーソル移動系のプラグインをcursor.luaにまとめる

--- dot_config/nvim/lua/plugins/cursor.lua
+++ dot_config/nvim/lua/plugins/cursor.lua
@@ -15,4 +15,16 @@ return {
 			)
 		end,
 	},
+	{
+    -- Move cursor without considering symbols
+		"kana/vim-smartword",
+		event = "VeryLazy",
+		config = function()
+			local options = { noremap = true }
+			vim.keymap.set("", "w", "<Plug>(smartword-w)", options)
+			vim.keymap.set("", "b", "<Plug>(smartword-b)", options)
+			vim.keymap.set("", "e", "<Plug>(smartword-e)", options)
+			vim.keymap.set("", "ge", "<Plug>(smartword-ge)", options)
+		end,
+	},
 }
