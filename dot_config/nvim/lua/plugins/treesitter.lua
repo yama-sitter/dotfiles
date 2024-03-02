30bbee066023a623e052199a55c5c639c7da19c1
yama-sitter <dadayamism@gmail.com>
2024-03-02 15:37:13 +0900

    treesitterの構文解析の対象を増やし、普段よく使うであろうファイルにシンタックスハイライトが当たるようにする

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,40 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = 'VeryLazy',
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'vim',
-        'lua',
-        'typescript',
-        'tsx',
-      },
-      highlight = {
-        enable = true,
-      },
-    }
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"vim",
+				"vimdoc",
+				"query",
+				"comment",
+				"diff",
+				"json",
+				"yaml",
+				"toml",
+				"tsv",
+				"csv",
+				"markdown",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"lua",
+				"html",
+				"css",
+				"typescript",
+				"javascript",
+				"tsx",
+				"ruby",
+				"dockerfile",
+			},
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
