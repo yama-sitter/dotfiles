3dcf28a1d2524b8d29dbcd03f733a3dd69b2d172
yama-sitter <dadayamism@gmail.com>
2024-03-07 21:56:24 +0900

    使わないGitsignsのキーバインドを削除

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -18,35 +18,6 @@ return {
 			current_line_blame_opts = {
 				delay = 300,
 			},
-
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
-
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
-
-				map("n", "<C-]>", function()
-					if vim.wo.diff then
-						return "]c"
-					end
-					vim.schedule(function()
-						gs.next_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
-				map("n", "<C-[>", function()
-					if vim.wo.diff then
-						return "[c"
-					end
-					vim.schedule(function()
-						gs.prev_hunk()
-					end)
-					return "<Ignore>"
-				end, { expr = true })
-			end,
 		},
 	},
 	{
