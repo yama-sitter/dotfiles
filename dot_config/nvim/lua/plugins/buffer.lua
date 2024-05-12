daee66851fb205dca7263a138cceee849332ec13
yama-sitter <dadayamism@gmail.com>
2024-05-12 11:59:18 +0900

    update: bufdel -> close-buffers に変更。BWipeout の存在が気に入った。またバッファを閉じる独自関数を修正し、C-q で簡単に操作できるようにする

--- dot_config/nvim/lua/plugins/buffer.lua
+++ dot_config/nvim/lua/plugins/buffer.lua
@@ -1,25 +1,32 @@
 return {
 	{
-		-- Close buffer with delete
-		"ojroques/nvim-bufdel",
+		-- Allows quickly delete multiple buffers
+		"kazhala/close-buffers.nvim",
 		event = "VeryLazy",
 		config = function()
-			function CloseBuffer()
-				if vim.bo.buftype == "terminal" then
+			local function close_buffer()
+				local file_name = vim.fn.expand("%:p")
+
+				if vim.bo.buftype == "terminal" or vim.bo.buftype == "acwrite" then
 					vim.cmd("bd!")
-        elseif vim.bo.buftype == "quickfix" then
+				elseif vim.bo.buftype == "quickfix" then
 					vim.fn.setqflist({}, "r")
 					vim.cmd("cclose")
-        elseif #vim.api.nvim_list_wins() > 1 then
-          vim.cmd("close")
+				elseif string.match(file_name, "^diffview.+") then
+					-- Allow to close the buffer which opened by diffview
+					vim.cmd("tabclose")
+				elseif #vim.api.nvim_list_wins() > 1 then
+					vim.cmd("close")
+					-- Allow to close the buffer which opened by gitsigns
+					vim.cmd("BWipeout! glob=gitsigns*")
 				else
-					vim.cmd("BufDel")
+					vim.cmd("BWipeout! this")
 				end
 			end
 
 			vim.keymap.set("n", "<C-l>", ":bn<CR>", { noremap = true, silent = true, desc = "Next buffer" })
 			vim.keymap.set("n", "<C-h>", ":bp<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
-			vim.keymap.set("n", "<C-q>", CloseBuffer, { noremap = true, silent = true, desc = "Close buffer" })
+			vim.keymap.set("n", "<C-q>", close_buffer, { noremap = true, silent = true, desc = "Close buffer" })
 		end,
 	},
 }
